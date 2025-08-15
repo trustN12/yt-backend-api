aa794b07933c9659dc59880c04431949c704a4e2
NABARUN <nbiswas100@outlook.com>
2025-08-15 21:30:01 +0530

    implemented signUp logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import fileUpload from "express-fileupload";
+import bodyParser from "body-parser"
 
 import { ConnectDB } from "./config/db.config.js";
 import  userRoutes  from "./routes/user.routes.js"
@@ -13,7 +14,14 @@ const app = express();
 ConnectDB();
 
 //! Middleware to parse JSON request bodies
-app.use(express.json());
+// app.use(express.json());
+
+app.use(bodyParser.json());
+
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: "./temp/"
+}))
 
 //todo: Middleware setup
 app.use('/api/v1/user', userRoutes)
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,46 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
 
+
+import User from "../schema/user.schema.js"
+import cloudinary from "../config/cloudinary.js"
 
 const router = express.Router();
 
-router.post("/signup", (req, res)=>{
-   res.send("Welcome to Youtube, Please signup!")
+router.post("/signup", async(req, res)=>{
+   try {
+      console.log("Request coming")
+      const hashPassword = await bcrypt.hash(req.body.password, 10);
+      console.log(hashPassword);
+      
+      const uploadImage = await cloudinary.uploader.upload(
+         req.files.logoUrl.tempFilePath
+      )
+
+      console.log("Image📷", uploadImage);
+      
+
+      const newUser = new User({
+         _id: new mongoose.Types.ObjectId,
+         email: req.body.email,
+         password: hashPassword,
+         channelName: req.body.channelName,
+         phone: req.body.phone,
+         logoUrl: uploadImage.secure_url,
+         logoId: uploadImage.public_id
+      })
+
+
+      let user = await newUser.save();
+
+
+      res.status(201).json({
+         user
+      })
+   } catch (error) {
+      console.log(error)
+   }
 })
 
 export default router;
